14d8b6720006aff54d8d068bcd10d422413bf6f5
A.C_Codes <62225871+ACabrera-Codes@users.noreply.github.com>
2021-05-01 20:37:16 -0400

    Contact form

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,29 +1,42 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 function Contact() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+emailjs.sendForm('gmail', 'template_i7b5yp2', e.target, 'user_Q1UGg9weXPWkwQAm0Vqvi')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset()
+}
   return (
     <div className="contact">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">          
-          {/* <form className="my-form" action="" method="post">
-            <div className="field">
-              <label for="name">Name</label>
-              <input type="text" name="name" id="name" required />
-            </div>
-            <div className="field">
-              <label for="email">Email</label>
-              <input type="email" name="email" id="email" required />
-            </div>
-            <div className="field">
-              <label for="name">Message</label>
-              <textarea rows="5" name="msg" id="msg" required />
-            </div>
-            <button>
-              Send
-            </button>
-            
-          </form> */}
+          <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your message" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send Message"></input>
+                        </div>
+                    </div>
+                </form>
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Let's Talk!</h1>
--- client/src/components/Submissions.jsx
+++ client/src/components/Submissions.jsx
@@ -1,12 +1,42 @@
 import React from "react";
+import emailjs from "emailjs-com";
 
 function Submissions() {
+  function sendEmail(e) {
+    e.preventDefault();
+
+emailjs.sendForm('gmail', 'template_i7b5yp2', e.target, 'user_Q1UGg9weXPWkwQAm0Vqvi')
+    .then((result) => {
+        console.log(result.text);
+    }, (error) => {
+        console.log(error.text);
+    });
+    e.target.reset()
+}
   return (
     <div className="submissions">
       <div class="container">
         <div class="row align-items-center my-5">
           <div class="col-lg-7">
-            
+          <form onSubmit={sendEmail}>
+                    <div className="row pt-5 mx-auto">
+                        <div className="col-8 form-group mx-auto">
+                            <input type="text" className="form-control" placeholder="Name" name="name"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <input type="text" className="form-control" placeholder="Your blog post title" name="subject"/>
+                        </div>
+                        <div className="col-8 form-group pt-2 mx-auto">
+                            <textarea className="form-control" id="" cols="30" rows="8" placeholder="Your Content" name="message"></textarea>
+                        </div>
+                        <div className="col-8 pt-3 mx-auto">
+                            <input type="submit" className="btn btn-info" value="Send submission"></input>
+                        </div>
+                    </div>
+                </form>
           </div>
           <div class="col-lg-5">
             <h1 class="font-weight-light">Submissions FAQ ✨</h1>
